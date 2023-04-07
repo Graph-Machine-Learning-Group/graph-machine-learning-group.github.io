16025e6e09ed59fc567208f7f1dd9a4c49a11231
marshka <ivan.marisca@hotmail.it>
2023-04-07 12:09:28 +0200

    Updated layout

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,9 +2,14 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const navIcon = document.getElementById('navbar-menu-icon');
+
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
+    navbarToggler.addEventListener('click', () => {
+        navIcon.classList.toggle('open');
+    });
     responsiveNavItems.map(function(responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
