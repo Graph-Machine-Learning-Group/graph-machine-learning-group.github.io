d187a5d2f71be95697dbc2a1634cbb3c7aaf8215
marshka <ivan.marisca@hotmail.it>
2024-04-29 18:39:27 +0100

    Added support for Bibtex

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,4 +18,38 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // Add copy button to code blocks
+    const copyButtonLabel = "Copy";
+
+    // use a class selector if available
+    let blocks = document.querySelectorAll("pre");
+
+    blocks.forEach((block) => {
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+        let button = document.createElement("button");
+
+        button.innerText = copyButtonLabel;
+        block.appendChild(button);
+
+        button.addEventListener("click", async () => {
+        await copyCode(block, button);
+        });
+    }
+    });
+
+    async function copyCode(block, button) {
+        let code = block.querySelector("code");
+        let text = code.innerText;
+
+        await navigator.clipboard.writeText(text);
+
+        // visual feedback that task is completed
+        button.innerText = "Copied";
+
+        setTimeout(() => {
+            button.innerText = copyButtonLabel;
+        }, 1000);
+    }
+
 });
